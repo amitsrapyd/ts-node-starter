853fba016d90aad36877739e42a8ce39fc5afcb9
Yariv Gilad <yariv.gilad@gmail.com>
2021-12-18 16:03:20 +0200

    feat: added prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import log from "@ajar/marker";
+
 import { saySomething } from "./myModule.js";
 
 const response = saySomething("hello");
-// const response = saySomething(22); // <-- will not compile when - "noEmitOnError": true 
-log. magenta (response);
 
-
+// const response = saySomething(22); // <-- will not compile when - "noEmitOnError": true
+log.magenta(response);
