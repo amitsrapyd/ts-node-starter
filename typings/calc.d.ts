28e3862e98cc809687c4433f5318a694d8dd4fde
Yariv Gilad <yariv.gilad@gmail.com>
2021-12-25 18:22:34 +0200

    build(ts): added include/exclude to tsconfig, since the tests folder containes  ts filesand is not within the src folder

---
+++ typings/calc.d.ts
@@ -0,0 +1,8 @@
+export declare function sum(...nums: number[]): number;
+/**
+ *
+ * @param m multiplier
+ * @param nums an array of numbers
+ * @returns an array of multiplied numbers
+ */
+export declare function multiply(m: number, ...nums: number[]): number[];
