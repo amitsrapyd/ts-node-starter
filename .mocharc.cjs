d256840b1388458d96900cb2942118c4cb9df756
Yariv Gilad <yariv.gilad@gmail.com>
2021-12-25 18:25:13 +0200

    chor: cleaned up .mocharc.cjs a bit...

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,11 +1,6 @@
 module.exports = {
-
-  // reporter: "spec",
-// //   "reporter-option": ["foo=bar", "baz=quux"], // array, not object
-
-  timeout: "2000",
-  
   extension: ["ts"],
   spec: "tests/**/*.spec.ts",
-  require: "@swc-node/register"
+  require: "@swc-node/register",
+  timeout: "2000"
 };
